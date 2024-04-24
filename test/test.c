88134dfe918519b2cea5a6b0fa8a48c81909c52a
ELIESSR <eliessermteran2005@gmail.com>
2024-04-24 16:55:04 +0200

    gnl prueba actualizada

--- test/test.c
+++ test/test.c
@@ -2,37 +2,22 @@
 
 int	main(void)
 {
-	char	*file;
 	int		fd;
 	char	*str;
+	int 	line;
 
-	file = "Prueba.txt";
-	fd = open(file, O_RDONLY);
-	str = get_next_line(fd);
-	while (str)
+	line = 1;
+	fd = open("prueba.txt", O_RDONLY);
+	while (line <= 10)
 	{
-		printf("%s", str);
 		str = get_next_line(fd);
+		if (!str)
+			break;
+		printf("%s", str);
+		line++;
 		free(str);
-		close(fd);
 	}
 	system("leaks -q a.out");
-	return(0);
-}
-int main()
-{
-	int fd = open("prueba3.txt", O_RDONLY);
-	char *manolo;
-	manolo = get_next_line(fd);
-	printf("%s", manolo);
-	manolo = get_next_line(fd);
-	printf("%s", manolo);
-	manolo = get_next_line(fd);
-	printf("%s", manolo);
-	manolo = get_next_line(fd);
-	printf("%s", manolo);
-	manolo = get_next_line(fd);
-	printf("%s", manolo);
 	close(fd);
-	return 0;
+	return (0);
 }
